59d8467f173eace34e8bbf3383ee9429a3677a5a
MorriV <kenixplay@gmail.com>
2022-12-29 18:30:47 -0600

    Cascaron y Sustantivos

---
+++ Diccionariov2/src/diccionariov2/Data.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package diccionariov2;
+
+/**
+ *
+ * @author Mr_Floppa
+ */
+public class Data {
+    String[] s = {"persona","comida","nube","hombre", "mujer","casa", "escuela"
+                , "vehiculo", "libro", "computadora", "laptop", "celular", "consola"
+                , "banca", "parque", "cine", "pueblo", "curso", "aplicación", "software"
+                , "moto", "coche", "camion", "mochila", "ropa", "sistema", "zapato"
+                , "tenis", "empresa", "camara", "perro", "gato", "alfombra", "edificio"
+                , "hospital", "mapache", "maestro", "maestra", "carro", "puerta"};
+    
+    String[] v = {"es", "esta"};
+}
